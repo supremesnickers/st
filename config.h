c004cae3729ec65a25d21bf6fac39c0b5f536516
hashidan <hoangseidel02@gmail.com>
2020-03-31 17:12:55 +0200

    fixes

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1;
-static float chscale = 1.15;
+static float chscale = 1;
 
 /*
  * word delimiter string
